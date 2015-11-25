b728cbfd1dafcdae7f0d2c1cbc4d37e9e7b030a1
Kirk Baker <kirk@lexicalintelligence.com>
2015-11-25 08:27:53 -0500

    connection pool

--- lexicalj/src/main/java/com/lexicalintelligence/LexicalClient.java
+++ lexicalj/src/main/java/com/lexicalintelligence/LexicalClient.java
@@ -18,38 +18,45 @@ package com.lexicalintelligence;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import com.lexicalintelligence.action.extract.ExtractRequest;
 
 public class LexicalClient {
 	protected final Log log = LogFactory.getLog(LexicalClient.class);
-	private static HttpClient httpClient = HttpClientBuilder.create().build();
+	private static final PoolingHttpClientConnectionManager cm;
+	
+	static {
+		cm = new PoolingHttpClientConnectionManager();
+		cm.setMaxTotal(16);
+	}
+	
+	private static CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm).build();
 	private static ObjectMapper mapper = new ObjectMapper();
-	
+
 	private String url;
-	
+
 	public LexicalClient(String url) {
 		System.setProperty("jsse.enableSNIExtension", "false");
 		this.url = url;
 	}
-	
+
 	public ExtractRequest prepareExtract() {
 		return new ExtractRequest(this);
 	}
-	
+
 	public String getUrl() {
 		return url;
 	}
-	
-	public HttpClient getHttpClient() {
+
+	public CloseableHttpClient getHttpClient() {
 		return httpClient;
 	}
-	
+
 	public ObjectMapper getObjectMapper() {
 		return mapper;
 	}
-	
 }
